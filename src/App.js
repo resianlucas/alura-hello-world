354f6d6fbda20b1fc5de094acc91bde7101f42bf
resianlucas <rm95781@fiap.com.br>
2025-04-14 03:13:17 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
-import Menu from "./componentes/Menus";
+
 
 
 
