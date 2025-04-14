1c89dd99f537a24286e5b4cdaeb21b16a7c57991
resianlucas <rm95781@fiap.com.br>
2025-04-14 03:06:59 -0300

    criando as primeiras paginas e componentes

---
+++ src/componentes/Menus/index.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import styles from "./Menu.module.css";
+
+export default function Menu() {
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <Link className={styles.link} to="/">Inicio</Link>
+                <Link className={styles.link} to="/sobremim">Sobre Mim</Link>
+            </nav>
+        </header>
+    )
+}
