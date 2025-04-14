71821a6093664b3d295c67300985f64e69f40fac
resianlucas <rm95781@fiap.com.br>
2025-04-14 04:43:22 -0300

    arrumando o menu de navegacao

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,12 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./Menu.module.css";
 
 export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} to="/">Inicio</Link>
-                <Link className={styles.link} to="/sobremim">Sobre Mim</Link>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => `
+                        ${styles.link}
+                        ${isActive ? styles.linkDestacado : ""}
+                    `}>
+                    Inicio
+                </NavLink>
+                <NavLink
+                    to="/sobremim"
+                    className={({ isActive }) => `
+                        ${styles.link}
+                        ${isActive ? styles.linkDestacado : ""}
+                    `}>
+                    Sobre Mim
+                </NavLink>
             </nav>
         </header>
     )
