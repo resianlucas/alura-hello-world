5e89e5e962d0353e9ad99512b00917d3c5e6aedd
resianlucas <rm95781@fiap.com.br>
2025-04-14 06:42:40 -0300

    Finalizando a pagina inicio inserindo os posts e o rodape

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,11 +1,22 @@
 import Banner from "componentes/Banner";
+import styles from './Inicio.module.css';
+import Post from "componentes/Post";
+import Rodape from "componentes/Rodape";
+
+import posts from 'json/posts.json';
 
 export default function Inicio() {
-    return(
-
+    return (
         <main>
             <Banner />
-            <h1>Olá Mundo!</h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                ))}
+            </ul>
+            <Rodape />
         </main>
     )
 }
